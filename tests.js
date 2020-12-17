85b67e7c4c31576cd6b2c47f0a5438ab56e37fc6
Sam Lansford <sam.j.lansford@gmail.com>
2020-12-17 16:35:28 -0600

    first test

--- tests.js
+++ tests.js
@@ -13,3 +13,19 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+
+describe('sayHello', function() {
+    it('should be a defined function', function() {
+        expect(typeof helloWorld).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof helloWorld()).toBe("string");
+    });
+    it('should return the string "Hello, Jane!" when executed', function() {
+        expect(helloWorld()).toBe("Hello, World!");
+    });
+    it("should never return 'undefined' when called", function() {
+        expect(helloWorld()).not.toBe(undefined);
+    });
+});
